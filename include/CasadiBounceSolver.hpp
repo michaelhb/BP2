80d28dd2b25f3d736460b03400e747374a287e46
Michael Bardsley <michael.bardsley@monash.edu>
2020-06-04 17:21:31 +1000

    Setting up some initial structure

---
+++ include/CasadiBounceSolver.hpp
@@ -0,0 +1,18 @@
+#ifndef BUBBLETESTER_CASADIBOUNCESOLVER_HPP_INCLUDED
+#define BUBBLETESTER_CASADIBOUNCESOLVER_HPP_INCLUDED
+
+#include <memory>
+#include <chrono>
+#include <exception>
+#include <math.h> 
+#include <casadi/casadi.hpp>
+
+namespace BubbleProfiler2 {
+
+class CasadiBounceSolver {
+
+};
+
+}
+
+#endif
